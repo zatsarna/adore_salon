91b084108d7c179e7f6de9ccec0ec746db351bd3
zatsarna <lyudazats@gmail.com>
2023-05-23 10:07:16 -0400

    adore salon

---
+++ script.js
@@ -0,0 +1,14 @@
+/*const stylist1El = document.querySelector(".stylist__photo--1");
+console.log(stylist1El);
+stylist1El.addEventListener("click", function () {
+  document
+    .getElementById("popup--1")
+    .classList.add("popup__visible", "popup__content__visible");
+});
+
+const btnClose = document.querySelector(".popup__close");
+btnClose.addEventListener("click", function () {
+  document
+    .getElementById("popup--1")
+    .classList.remove("popup__visible", "popup__content__visible");
+});*/
